c5002143926715a90116312a6af77f3d99615732
Jovani Pink <jovanirosado@hotmail.com>
2021-01-11 17:22:57 -0500

    Updated to "ES6"

--- JavaScript/stack.js
+++ JavaScript/stack.js
@@ -1,30 +1,38 @@
 // Implementing a Stack Data Structure
 // Implementation is over the primitive Array Structural Type
 
-// ES5
+// ES6
+
+// No real way to re-implement this stack usage in ES6 since I wanted to
+// do both show the stack structure and solve for a palindrome.
+// But solving the palindrome in ES6 is below...
 
 // Initialize an empty Array to hold elements in the stack
-var letters = [];
+let letters = [];
 
 // Random palindrome word we are checking
-var word = "bob";
-var rword = "";
+let word = "bob";
+let rword = word.split("").reverse();
 
 // Put letters of word into a stack
-for (var i = 0; i < word.length; i++) {
-  letters.push(word[i]);
-}
+// for (var i = 0; i < word.length; i++) {
+//   letters.push(word[i]);
+// }
 
 // Pop off the stack in reverse order
-for (var i = 0; i < word.length; i++) {
-  rword += letters.pop();
-}
+// for (var i = 0; i < word.length; i++) {
+//   rword += letters.pop();
+// }
 
 // Now we check if the word and reversed word match...
-if (rword === word) {
-  console.log(word + "is a palindrome)");
-} else {
-  console.log(word + "is not a palindrome");
-}
+// if (JSON.stringify(rword) === JSON.stringify(word)) {
+//   console.log(word + "is a palindrome)");
+// } else {
+//   console.log(word + "is not a palindrome");
+// }
 
-// Again I was just using the built in Array methods to make a stack.
+JSON.stringify(rword) === JSON.stringify(word.split(""))
+  ? console.log(strWord + " is a palindrome")
+  : console.log(strWord + " is not a palindrome");
+
+// This is really poorly coded. Sorry.
